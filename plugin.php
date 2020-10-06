95eea960d92f8afeb5a4d9b512b8667263dd6713
Edi <egoetschel@clickwork.ch>
2020-10-06 14:16:59 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,15 +3,15 @@
 class pluginCookiesBanner extends Plugin {
 
 
-  public function init() {
-	  $this->dbFields = array(
+	public function init() {
+		$this->dbFields = array(
 			'text'=>'',
 			'code'=>''
 		);
 	}
 
 	public function form() {
-	  global $L;
+		global $L;
 
 		$html = '<div>';
 		$html .= '<label>'.$L->get('Text').'</label>';
@@ -27,34 +27,35 @@ class pluginCookiesBanner extends Plugin {
 	}
 
 	public function siteBodyBegin(){
+	global $L;
 
-	  $text = $this->getValue('text');
-	  $text = htmlspecialchars_decode($text);
+	$text = $this->getValue('text');
+	$text = htmlspecialchars_decode($text);
 
     $html = '<link rel="stylesheet" href="'.HTML_PATH_PLUGINS.'cookies-banner/css/cookies-eu-banner.default.css">'.PHP_EOL;
-	  $html .= '<div id="cookies-eu-banner" style="display: none;">'.PHP_EOL;
-	  $html .= nl2br($text).PHP_EOL;
-    $html .= '<button id="cookies-eu-reject">Reject</button>'.PHP_EOL;
-    $html .= '<button id="cookies-eu-accept">Accept</button>'.PHP_EOL;
-	  $html .= '</div>'.PHP_EOL;
+	$html .= '<div id="cookies-eu-banner" style="display: none;">'.PHP_EOL;
+	$html .= nl2br($text).PHP_EOL;
+    $html .= '<button id="cookies-eu-reject">'.$L->get('Reject').'</button>'.PHP_EOL;
+    $html .= '<button id="cookies-eu-accept">'.$L->get('Accept').'</button>'.PHP_EOL;
+	$html .= '</div>'.PHP_EOL;
 
-	  return $html;
+	return $html;
 
 	}
 
 	public function siteBodyEnd(){
 
-	  $code = $this->getValue('code');
-	  $code = htmlspecialchars_decode($code);
+	$code = $this->getValue('code');
+	$code = htmlspecialchars_decode($code);
 
-	  $html = '<script src="'.HTML_PATH_PLUGINS.'cookies-banner/js/cookies-eu-banner.js"></script>'.PHP_EOL;
-	  $html .= '<script>'.PHP_EOL;
+	$html = '<script src="'.HTML_PATH_PLUGINS.'cookies-banner/js/cookies-eu-banner.js"></script>'.PHP_EOL;
+	$html .= '<script>'.PHP_EOL;
     $html .= 'new CookiesEuBanner(function () {'.PHP_EOL;
     $html .= $code;
     $html .= '}, true);'.PHP_EOL;
-	  $html .= '</script>'.PHP_EOL;
+	$html .= '</script>'.PHP_EOL;
 
-	  return $html;
+	return $html;
 
 	}
 }
